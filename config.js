0f6f1797bd60c326738b5c46d6a1f4bb8bec2b61
Fan <fansc@shinetechchina.com>
2020-01-06 13:45:59 +0800

    问题修复： add siturl

--- config.js
+++ config.js
@@ -24,6 +24,7 @@ const config = {
 		"title": "帮助中心 | 趣show",
 		"description": "趣show帮助中心 ",
 		"ogImage": null,
+		"siteUrl":'https://www.qushow365.com/help',
 		"docsLocation": "https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content",
 		"favicon": "https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg"
 	},
